03e3e624cbf52c21dc268d1179e8f5594cb8f539
areel321 <112980267+areel321@users.noreply.github.com>
2024-10-01 22:06:54 -0400

    updates

--- Project3/flashcardsProject2/src/App.jsx
+++ Project3/flashcardsProject2/src/App.jsx
@@ -4,15 +4,6 @@ import Flashcard from './flashcard.jsx';
 
 
 function App() {
-  const [index, setIndex] = useState(0)
-  const handleNext = () => {
-    
-    setIndex((index) => (index + 1) % flashcards.length);
-  };
-  const handleBack = () => {
-    setIndex((index) => (index - 1 + flashcards.length) % flashcards.length);
-  };
-  
   const flashcards = [
     {front: 'je',
       back: 'i',
@@ -44,6 +35,63 @@ function App() {
     },
   ]
 
+  const [index, setIndex] = useState(0);
+  const [answer, setAnswer] = useState('');
+  const [resultColor, setResultColor] = useState({});
+  const [shuffle, setShuffle] = useState(false);
+  const [cards, setCards] = useState(flashcards);
+  const [streak, setStreak] = useState(0);
+
+  const handleNext = () => {
+    resetInput();
+    setIndex((index) => (index + 1) % flashcards.length);
+  };
+  const handleBack = () => {
+    resetInput();
+    setIndex((index) => (index - 1 + flashcards.length) % flashcards.length);
+  };
+
+  const resetInput = () => {
+    setAnswer('');
+    setResultColor({}); 
+  };
+
+  const checkAnswer = () => {
+    if (answer === flashcards[index].back) {
+      setResultColor({ borderColor: 'green' }); 
+      setStreak((streak) => streak + 1);
+    } else {
+      setResultColor({ borderColor: 'red' }); 
+      setStreak(0);
+    }
+  };
+
+  const handleShuffle = () => {
+    resetInput();
+    setShuffle(!shuffle);
+
+    if (!shuffle) {
+      let shuffledArr = [...flashcards]
+      let currentIndex = shuffledArr.length;
+      while (currentIndex != 0) {
+
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [shuffledArr[currentIndex], shuffledArr[randomIndex]] = [
+          shuffledArr[randomIndex], shuffledArr[currentIndex]];
+      }
+      setCards(shuffledArr); 
+      setIndex(0); 
+    } else {
+      setCards(flashcards); 
+      setIndex(0); 
+    }
+
+  }
+
+  
+  
+
 
   return (
     <>
@@ -52,13 +100,27 @@ function App() {
       <p>Flashcard set of French pronouns to English pronouns</p>
       <p>7 cards in set, colors indicate difficulty level</p>
     </div>
-    <div class="flashcards">
-
-      <Flashcard flashcards={flashcards} index={index}/>
-      <div class="buttons">
+    <div className="flashcards">
+      <Flashcard flashcards={cards} index={index}/>
+      <div className="answer">
+        <input 
+          type="text" 
+          value={answer} 
+          onChange={(e) => setAnswer(e.target.value)} 
+          placeholder="guess here!"
+          style={resultColor} 
+        />
+        <button onClick={checkAnswer}>Check Answer</button>
+      
+      </div>
+      <div className="buttons">
         <button onClick={handleBack}>Back</button>
         <button onClick={handleNext}>Next</button>
+        <button onClick={handleShuffle}>Shuffle</button>
         
+      </div>
+      <div>
+        <h3>Current Streak: {streak} </h3>
       </div>
 
     </div>
